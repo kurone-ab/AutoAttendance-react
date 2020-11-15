b02465c964ad9b6d3dd7da78a1b0064d1f9a3b13
AiRini <ygnaiih1680@mju.ac.kr>
2020-11-15 17:12:09 +0900

    deploy

--- src/AxiosUtil.js
+++ src/AxiosUtil.js
@@ -2,6 +2,9 @@ import Axios from "axios";
 import {Agent} from 'https'
 
 export const AxiosInstance = Axios.create({
+    httpsAgent: new Agent({
+       rejectUnauthorized: false
+    }),
     withCredentials: true,
     baseURL: "https://3.35.0.42:8080/"
 })
